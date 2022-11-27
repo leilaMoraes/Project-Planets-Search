184803d01f64050d3d385c3a4076534d897d34d1
Leila Moraes <leila.violino@gmail.com>
2022-11-27 17:10:10 -0300

    Adiciona `loading`

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,39 +6,42 @@ const tableHead = ['Name', 'Rotation Period', 'Orbital Period', 'Diameter', 'Cli
   'Edited', 'URL'];
 
 function Table() {
-  const { data } = useContext(PlanetsContext);
+  const { data, loading } = useContext(PlanetsContext);
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            { tableHead.map((head, i) => (
-              <th className="table-th" key={ i }>
-                { head }
-              </th>)) }
-          </tr>
-        </thead>
-        <tbody>
-          {data.length > 0 && data.map((planets) => (
-            <tr key={ planets.name }>
-              <td data-testid="planet-name">{planets.name }</td>
-              <td>{planets.rotation_period }</td>
-              <td>{planets.orbital_period}</td>
-              <td>{planets.diameter}</td>
-              <td>{planets.climate}</td>
-              <td>{planets.gravity}</td>
-              <td>{planets.terrain}</td>
-              <td>{planets.surface_water}</td>
-              <td>{planets.population}</td>
-              <td>
-                {planets.films.map((film, i) => <p key={ i }>{film}</p>)}
-              </td>
-              <td>{planets.created}</td>
-              <td>{planets.edited}</td>
-              <td>{planets.url}</td>
-            </tr>))}
-        </tbody>
-      </table>
+      {loading
+        ? <h1>Loading...</h1>
+        : (
+          <table>
+            <thead>
+              <tr>
+                { tableHead.map((head, i) => (
+                  <th className="table-th" key={ i }>
+                    { head }
+                  </th>)) }
+              </tr>
+            </thead>
+            <tbody>
+              {data.length > 0 && data.map((planets) => (
+                <tr key={ planets.name }>
+                  <td data-testid="planet-name">{planets.name }</td>
+                  <td>{planets.rotation_period }</td>
+                  <td>{planets.orbital_period}</td>
+                  <td>{planets.diameter}</td>
+                  <td>{planets.climate}</td>
+                  <td>{planets.gravity}</td>
+                  <td>{planets.terrain}</td>
+                  <td>{planets.surface_water}</td>
+                  <td>{planets.population}</td>
+                  <td>
+                    {planets.films.map((film, i) => <p key={ i }>{film}</p>)}
+                  </td>
+                  <td>{planets.created}</td>
+                  <td>{planets.edited}</td>
+                  <td>{planets.url}</td>
+                </tr>))}
+            </tbody>
+          </table>)}
     </div>
   );
 }
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -5,18 +5,21 @@ import fetchApi from '../services/fetchApi';
 
 export default function PlanetsProvider({ children }) {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getData = async () => {
+      setLoading(true);
       const useFetch = await fetchApi();
       setData(useFetch);
+      setLoading(false);
     };
     getData();
   }, []);
 
   const values = useMemo(() => ({
-    data,
-  }), [data]);
+    data, loading,
+  }), [data, loading]);
 
   return (
     <PlanetsContext.Provider value={ values }>
