8ab021009a9a3b7f31f4bd2fa7c111cce15b646e
Leila Moraes <leila.violino@gmail.com>
2022-11-27 14:39:42 -0300

    Cria a tabela que renderiza as informações da API

---
+++ src/components/Table.js
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react';
+import PlanetsContext from '../context/PlanetsContext';
+
+const tableHead = ['Name', 'Rotation Period', 'Orbital Period', 'Diameter', 'Climate',
+  'Gravity', 'Terrain', 'Surface Water', 'Population', 'Films', 'Created',
+  'Edited', 'URL'];
+
+function Table() {
+  const { data } = useContext(PlanetsContext);
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            { tableHead.map((head, i) => (
+              <th className="table-th" key={ i }>
+                { head }
+              </th>)) }
+          </tr>
+        </thead>
+        <tbody>
+          {data.length > 0 && data.map((planets) => (
+            <tr key={ planets.name }>
+              <td data-testid="planet-name">{planets.name }</td>
+              <td>{planets.rotation_period }</td>
+              <td>{planets.orbital_period}</td>
+              <td>{planets.diameter}</td>
+              <td>{planets.climate}</td>
+              <td>{planets.gravity}</td>
+              <td>{planets.terrain}</td>
+              <td>{planets.surface_water}</td>
+              <td>{planets.population}</td>
+              <td>{planets.films}</td>
+              <td>{planets.created}</td>
+              <td>{planets.edited}</td>
+              <td>{planets.url}</td>
+            </tr>))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
